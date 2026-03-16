62112292f75c6112ffd50842cae2fa06f86199df
jkgb92-png <jkgb92@gmail.com>
2026-03-16 01:16:20 -0400

    Create App component that imports DicyaninFilter

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import DicyaninFilter from './components/DicyaninFilter';
+
+const App = () => {
+    return (
+        <div>
+            <h1>All Seeing Glass</h1>
+            <DicyaninFilter />
+        </div>
+    );
+};
+
+export default App;
